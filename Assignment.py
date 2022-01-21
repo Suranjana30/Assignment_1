f26d271ce4c874d79709dda7da6bdd05d60e763d
Suranjana30 <90706866+Suranjana30@users.noreply.github.com>
2022-01-21 12:11:45 +0530

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,3 @@
+information={'name':'Suranjana','age':'19','education':'CSE(DS) Undergraduate','roll':'01','country':'india','State':'West Bengal'}
+for i,j in information.items():
+    print(f"the key is:{i}      The value is:{j}")
